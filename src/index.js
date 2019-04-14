8ae072df7431612a8bd07c49ce93982e6ca8ea9c
luzhongkuan <luzhongk@126.com>
2019-04-14 11:51:55 +0800

    导出mysqlHelper

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const resolve = require('./resolve.js')
 const fileDisplay = require('./fileDisplay')
 const copyDir = require('./copyDir')
 const logger = require('./logger')
+const MysqlHelper = require('./MysqlHelper')
 
 module.exports = {
   resolve,
   fileDisplay,
   copyDir,
-  logger
+  logger,
+  MysqlHelper
 }
